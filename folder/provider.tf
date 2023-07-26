d59ef6b327bc3d8673b6053cf63701b9c35d99a5
sameer-shaik <41112479+sameer-shaik@users.noreply.github.com>
2023-07-26 12:18:19 +0530

    commented access and secret key

--- folder/provider.tf
+++ folder/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA2AON6PJFKNKKE35F"
-  secret_key = "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"
+  # access_key = "AKIA2AON6PJFKNKKE35F"
+  # secret_key = "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"
 }
